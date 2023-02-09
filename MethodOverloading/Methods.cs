853d8a12fe8f4fd8b58b889f6bc2754eaf24e56a
Adam Measel <ameasel55@gmail.com>
2023-02-09 08:56:03 -0600

    initial commit

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,32 @@
+﻿namespace MethodOverloading
+{
+    public class Methods
+    {
+       public static int Add(int x, int y)
+        {
+            return (x + y);
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return (x + y);
+        }
+       public static string Add(int x, int y, bool z)
+        {
+            int answer = x + y;
+
+            if (z && answer > 1)
+            {
+                return $"{answer} dollars";
+            }
+            else if (z && answer == 1)
+            {
+                return $"{answer} dollar";
+            }
+            else
+            {
+                return $"error";
+            }
+        }
+        
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace MethodOverloading
+﻿using System;
+namespace MethodOverloading
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Methods.Add(4, 12);
+			Methods.Add(4.7m, 5.9m);
+			Methods.Add(7, 20, false);
+		}
+	}
 }
+
